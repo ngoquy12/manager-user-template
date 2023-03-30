9a21dd2bd771ff79a5a41767cf3ffd2e3f17a927
nvquy <ngoq753429@gmail.com>
2023-03-30 14:02:33 +0700

    update template

---
+++ index.js
@@ -0,0 +1,24 @@
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("one"), 1000);
+  });
+  const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("two"), 2000);
+  });
+  const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("three"), 3000);
+  });
+  const p4 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("four"), 4000);
+  });
+//   const p5 = new Promise((resolve, reject) => {
+//     resolve(new Error("reject"));
+//   });
+  
+  // Using .catch:
+  Promise.all([p1, p2, p3, p4])
+    .then((values) => {
+      console.log(values);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
